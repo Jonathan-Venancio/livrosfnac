bd43e03cd94e4393ea8ca223814235143bac9a93
Jonathan Venancio <jonathan.ghabriel@gmail.com>
2026-07-10 22:05:42 +0100

    .

---
+++ app/app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from PIL import Image
+
+from app.barcode_utils import gerar_barcode
+from app.ocr import ler_imagem
+
+st.set_page_config(page_title="Leitor de EAN")
+
+st.title("📚 Leitor de Lista de Livros")
+
+arquivo = st.file_uploader(
+    "Envie uma foto da folha",
+    type=["jpg", "png", "jpeg"]
+)
+
+if arquivo:
+
+    imagem = Image.open(arquivo)
+
+    st.image(imagem)
+
+    produtos = ler_imagem(imagem)
+
+    st.success(f"{len(produtos)} produtos encontrados")
+
+    for produto in produtos:
+
+        st.divider()
+
+        st.subheader(produto["descricao"])
+
+        st.write(produto["ean"])
+
+        codigo = gerar_barcode(produto["ean"])
+
+        st.image(codigo)
